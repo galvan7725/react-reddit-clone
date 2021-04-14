1c4b354a4a4d6d4bfca716815bcf67c71a2e952f
galvan9001 <46407403+galvan9001@users.noreply.github.com>
2021-04-14 16:35:25 -0500

    signup form alert

--- components/signup/SignupBody.tsx
+++ components/signup/SignupBody.tsx
@@ -1,33 +1,64 @@
 import React,{useState} from 'react';
-import { Col, Row , Button, Container, FormGroup} from 'reactstrap';
-import { Formik, Form, Field } from 'formik';
+import { Col, Row , Button, Container, FormGroup, Spinner} from 'reactstrap';
+import { Formik, Form, Field, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
 import MyInput from './FormikInput';
 import Link from 'next/link';
 import { signUp } from './ApiCalls';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
+const MySwal = withReactContent(Swal);
+
+interface UserData {
+    email: string,
+    username: string,
+    password: string
+}
+
+const showAlert = (success : boolean) : void =>{
+    if(success){
+        MySwal.fire({
+            title: 'Success!',
+            footer:'Reddit Clone',
+            text: 'User registered successfully',
+            icon:'success',
+            timer: 2000
+        })
+    }else{
+        MySwal.fire({
+            title: 'Error!',
+            footer:'Reddit Clone',
+            text:'Error, something went wrong. Please try again.',
+            icon:'success',
+            timer: 2000
+        })
+    }
+}
 
 
 const Signup = () : JSX.Element => {
 
     const [loadign, setLoadign] = useState<boolean>(false);
-    const [erros, setErros] = useState<String>("");
+    const [errors, setErrors] = useState<String>("");
     const signupSchema = Yup.object().shape({
         email :Yup.string().email('Invalid Email').required('Required'),
         username :Yup.string().min(5,'Too short').max(50,'Too long').required('Required'),
         password :Yup.string().min(5,'Password in too short').max(50,'Password in too long').required('Password is required')
     })
 
-    const signup = async (values, actions) : Promise<any> => {
-        console.log('Signup works'+JSON.stringify(values));
+    const signup = async (values : UserData , actions: FormikHelpers<UserData>) : Promise<any> => {
+        //console.log('Signup works'+JSON.stringify(values));
         setLoadign(true);
         try {
             const response = await signUp(values);
             if (response.error) {
                 setLoadign(false);
-                setErros(response.error);
+                setErrors(response.error);
+                showAlert(false);
             }else{
                 setLoadign(false);
+                showAlert(true);
                 console.log("Register Success!");
             }
         } catch (error) {
@@ -61,7 +92,8 @@ const Signup = () : JSX.Element => {
                                   <Field type="text" label="Username" name="username" component={MyInput} />
                                   <Field type="password" label="Password" name="password" component={MyInput} />
                                   <div style={{display:'flex'}}>
-                                    <Button type="submit" color="success" >Register</Button>
+                                    <Button type="submit" color="success" disabled={loadign} >Register</Button>
+                                    {loadign ? <Spinner color="primary" /> : null}
                                     <label style={{marginLeft:'auto'}} >Existing user?</label>
                                     <Link  href="/login" >Login</Link>
                                   </div>
