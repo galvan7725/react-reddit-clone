cb3b9910faddd7393201a02412098d65027205d0
galvan9001 <46407403+galvan9001@users.noreply.github.com>
2021-04-27 17:48:51 -0500

    protect route

--- pages/create_post.tsx
+++ pages/create_post.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
 import CreatePostBody from '../components/posts/CreatePostBody';
 import Layout from '../components/shared/Layout';
+import Security from '../security/Security';
+import Loading from '../components/shared/Loading';
 
 const createPost = () => {
+
+    const router = useRouter();
+
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const security = new Security();
+        if(!security.verifyToken()){
+            setLoading(false);
+            router.push('/login');
+        }else{
+            setLoading(false);
+        }
+        return () => {
+          setLoading(true);  
+        }
+    }, [])
+
     return (
         <Layout pageTitle="Create Post">
-            <CreatePostBody />
+            <>
+                {loading ? (<Loading/>) 
+                : 
+                (<CreatePostBody />)}
+            </>
         </Layout>
     )
 }
