d4f4dd848b871147662e71817723eebe2e92579a
galvan9001 <46407403+galvan9001@users.noreply.github.com>
2021-04-28 19:45:48 -0500

    Minor change custom formik input

--- components/shared/FormikInput.tsx
+++ components/shared/FormikInput.tsx
@@ -8,17 +8,7 @@ const FormikInput =  ({
 }) : JSX.Element =>(
     <FormGroup>
         <Label for={props.id}>{props.label}</Label>
-        <Input {...props} {...fields} invalid={Boolean(touched[fields.name] && errors[fields.name])}>
-            {props.type === 'select' && props.optionsselect.length > 0 ? (
-                 <>
-                    {props.optionsselect.map((option, i) =>(
-                        <><option value={option.name}>{option.name}</option></>
-                    ))}
-                 </>   
-            ) : (
-                <></>
-            )}
-        </Input>
+        <Input {...props} {...fields} invalid={Boolean(touched[fields.name] && errors[fields.name])}/>
         {touched[fields.name] && errors[fields.name] ? <FormFeedback>{errors[fields.name]}</FormFeedback> : null}
     </FormGroup>
 );
