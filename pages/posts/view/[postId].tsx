4f4818f9c7231480f7bf7cd3ff55209e15db6d91
galvan9001 <46407403+galvan9001@users.noreply.github.com>
2021-05-03 12:39:23 -0500

    single post page

--- pages/posts/view/[postId].tsx
+++ pages/posts/view/[postId].tsx
@@ -1,10 +1,37 @@
+import { useRouter } from 'next/router';
 import React from 'react'
+import SinglePostBody from '../../../components/posts/SinglePostBody';
+import Layout from '../../../components/shared/Layout';
+import Loading from '../../../components/shared/Loading';
+import { PostStateProvider } from '../../../context/PostContext';
 
-const SinglePost = () => {
+const SinglePost = () : JSX.Element => {
+
+    const router = useRouter();
+    const {postId} = router.query;
+    console.log(postId);
+    /*
+    useEffect(() => {
+        
+        if(postId == null || postId === undefined){
+            console.log('null');
+        }else{
+            console.log(postId);
+            setPostId(+postId);
+        }
+    
+    }, [])
+    */
     return (
-        <div>
-            <h1>SinglePost</h1>
-        </div>
+        <Layout pageTitle="Post">
+            {postId == null || postId == undefined ? <Loading/> :
+             <>
+             <PostStateProvider>
+                <SinglePostBody  {...{postId:+postId}} />
+             </PostStateProvider>
+             </>
+             } 
+        </Layout>
     )
 }
 
---
+++ pages/posts/view/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = (): JSX.Element=> {
+    return (
+        <div>
+            <h1>Post without postId</h1>
+        </div>
+    )
+}
+
+export default index;
