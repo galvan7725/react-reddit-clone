a5207ae2da47fcce3a014a4b811f3902d09774dd
galvan9001 <46407403+galvan9001@users.noreply.github.com>
2021-05-11 17:48:18 -0500

    Connect to the api server

--- components/login/ApiCalls.ts
+++ components/login/ApiCalls.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { JWTData, LoginData } from '../interfaces';
 
 
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'https://springredditclone2.herokuapp.com/api';
+//const BASE_URL = 'http://localhost:8080/api';
 
 
 export const Login = async(loginData : LoginData) : Promise<any> => {
